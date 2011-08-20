b633a57ec98b063ae9c6b6e4a5702baadca1ae9f
Lab Work 7th Sem <labwork@shikhar-laptop.(none)>
2011-08-20 14:38:59 +0530

    removed some files

--- fulladder.vhd
+++
@@ -1,9 +0,0 @@
-entity fulladder is
-    port(x,y,cin:in bit;s,cout:out bit);
-end entity;
-
-architecture fulladder_arch of fulladder is
-begin
-    s <= x xor y xor cin;
-    cout <= (x and y) or (y and cin) or (x and cin);
-end fulladder_arch;
--- fulladdertb.vhd
+++
@@ -1,5 +0,0 @@
-entity fulladder_tb is
-end fulladder_tb;
-
-architecture fulladder_tb_arch of fulladder_tb is
-    component adder is
