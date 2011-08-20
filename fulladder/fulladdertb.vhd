8a9602043503f56b155786b8e146381ed70551f3
Lab Work 7th Sem <labwork@shikhar-laptop.(none)>
2011-08-20 16:19:42 +0530

    Full adder compiled and tested with ghdl and gtkwave

--- fulladder/fulladdertb.vhd
+++ fulladder/fulladdertb.vhd
@@ -2,4 +2,19 @@ entity fulladder_tb is
 end fulladder_tb;
 
 architecture fulladder_tb_arch of fulladder_tb is
-    component adder is
+    component fulladder is
+        port(x,y,cin:in bit;s,cout:out bit);
+    end component;
+    signal ain,bin,carryin,sum,carryout:bit;
+    begin
+        inst0: fulladder port map(ain,bin,carryin,sum,carryout);
+    process
+        begin
+            ain <= '0';
+            bin <= '0';
+            carryin <= '1';
+            wait for 20 ns;
+            bin <= '1';
+            wait for 20 ns;
+    end process;
+end fulladder_tb_arch;
